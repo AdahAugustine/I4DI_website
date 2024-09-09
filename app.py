bd008c6f8f420dd82bd9b2c587c6a214c78dc6ba
AdahAugustine <aadah@i4di.org>
2024-09-09 17:13:34 +0100

    new_correction

--- app.py
+++ app.py
@@ -1,26 +1,30 @@
 from flask import Flask, render_template, request, jsonify
+# import time
 from flask_cors import CORS
 from dotenv import load_dotenv, find_dotenv
+import time
 from chat import get_response
+
 
 load_dotenv(find_dotenv())
 app = Flask(__name__)
 CORS(app)
 
+app = Flask(__name__)
 
-@app.get("/")
-def index_get():
-    return render_template("base.html")
-
-@app.post("/predict")
-def predict():
-    text=request.get_json().get("message")
-    response=get_response(text)
-    message= {"answer": response}
-    return jsonify(message)
+@app.route("/")
+def index():
+    return render_template('chat.html')
 
 
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    return get_response(input)
+    # time.sleep(10)
+    # return "Yes,It works"
 
-    
-if __name__=="__main__":
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run()
